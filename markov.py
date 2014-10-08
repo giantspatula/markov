c107cab8404e5939ac1443bcc0320c7ecd7ffcb5
giantspatula <cwmalloy@gmail.com>
2014-10-07 23:12:19 -0700

    Markov chain by sentence, markov chain by entire corpus. Still fiddling with corpus punctuation.

--- markov.py
+++ markov.py
@@ -3,41 +3,56 @@
 import sys
 import random
 import string
+import re
 
 def make_chains(corpus):
     """Takes an input text as a string and returns a dictionary of
     markov chains."""
-    #using corpus instead of "file_string" b/c corpus is the generic argument used for this block of code.  It is replaced w/ whatever is passed in the main function.''
-
+    
     chain_dict = {}
 
     sentence_list = massage_corpus(corpus)
     for sentence in sentence_list:
-        sentence = sentence.strip()
-        list_of_words = sentence.split(" ")
-        last_tuple = len(list_of_words) - 2 
-        for x in range(0, last_tuple):
-           key_tuple = (list_of_words[x], list_of_words[x + 1])
-           value_list = [list_of_words[x + 2]]
-           chain_dict[key_tuple] = chain_dict.get(key_tuple, []) + value_list
-
-        key_tuple = (list_of_words[last_tuple],list_of_words[last_tuple + 1])
-        value_list = ["."]
-        chain_dict[key_tuple] = chain_dict.get(key_tuple, []) + value_list
+        populate_dict(sentence, chain_dict)
 
     return chain_dict
 
+def populate_dict(sentence, dict):
+
+    list_of_words = sentence.split()
+    if len(list_of_words) >= 2:
+        last_tuple = len(list_of_words) - 2 
+        for x in range(0, last_tuple):
+            key_tuple = (list_of_words[x], list_of_words[x + 1])
+            value_list = [list_of_words[x + 2]]
+            dict[key_tuple] = dict.get(key_tuple, []) + value_list
+
+        key_tuple = (list_of_words[last_tuple],list_of_words[last_tuple + 1])
+        value_list = ["."]
+        dict[key_tuple] = dict.get(key_tuple, []) + value_list
+    #else:
+        #sentence of length 1. Ingore word. 
+
+
 def massage_corpus(corpus):
-    """Takes corpus, returns list of sentences"""
+    """Takes corpus, returns list of sentences with cleaned words"""
 
-    #probably be cleaned up with maketrans and translate
-
-    corpus = corpus.replace("\n", " ").replace("!", ".").replace("?", ".").replace('\'', "").replace("(", "").replace(")", "")
-    return  corpus.split(".")
+    corpus = corpus.replace("\n", " ").replace("-", " ").replace(":", " ").replace("!", ".").replace("?", ".").replace("\r", " ").replace("\t", " ").replace("  ", " ")
+    valid_punctuation = string.letters + "." + " " + "," + ";" + "'"
+    clean_text = ""
+    for char in corpus:
+        if char in valid_punctuation:
+            clean_text = clean_text + char
+    #print clean_text
+    sentences = clean_text.split(".")
+    # for sentence in sentences:
+    #     sentence = sentence.strip()
+    return sentences
 
 def make_text(chains):
     """Takes a dictionary of markov chains and returns random text
     based off an original text."""
+
     random_value = None
     key_list = chains.keys()
     random_tuple = random.choice(key_list)
@@ -48,15 +63,18 @@ def make_text(chains):
         markov_sent.append(random_value)
         random_tuple = (random_tuple[1], random_value)
 
-    # print markov_sent
+    return custom_join(markov_sent)
 
-    #writing a join as a loop
-    #markov_sent is a list! We're going to concatinate the strings in this list. 
-    # for word in markov_sent:
+def custom_join(list):
 
-    str = " "
-    markov_string = str.join(markov_sent)
-    return markov_string
+    first_word = list[0]
+    joined_string = string.capitalize(first_word)
+    for x in range(1,len(list)-1):
+        joined_string = joined_string + " " + list[x]
+    joined_string = joined_string + "."
+
+    return joined_string
+
 
 def main():
     args = sys.argv
