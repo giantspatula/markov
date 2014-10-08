ee91c698fa9ceaaae8aa94467ed09dd5891cb4c3
Hackbright Student <info@hackbrightacademy.com>
2014-10-07 17:40:35 -0700

    Working Markov

--- markov.py
+++ markov.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
 import sys
+import random
+import string
 
 def make_chains(corpus):
     """Takes an input text as a string and returns a dictionary of
@@ -14,17 +16,19 @@ def make_chains(corpus):
         sentence = sentence.strip()
         list_of_words = sentence.split(" ")
         last_tuple = len(list_of_words) - 2 
-        for x in range(0, len(list_of_words) - 2):
+        for x in range(0, last_tuple):
            key_tuple = (list_of_words[x], list_of_words[x + 1])
            value_list = [list_of_words[x + 2]]
            chain_dict[key_tuple] = chain_dict.get(key_tuple, []) + value_list
+
         key_tuple = (list_of_words[last_tuple],list_of_words[last_tuple + 1])
         value_list = ["."]
         chain_dict[key_tuple] = chain_dict.get(key_tuple, []) + value_list
-    print chain_dict
+
+    # print chain_dict
 
     return chain_dict
-    
+
 def massage_corpus(corpus):
     """Takes corpus, returns list of sentences"""
 
@@ -34,7 +38,26 @@ def massage_corpus(corpus):
 def make_text(chains):
     """Takes a dictionary of markov chains and returns random text
     based off an original text."""
-    return "Here's some random text."
+    random_value = None
+    key_list = chains.keys()
+    random_tuple = random.choice(key_list)
+    markov_sent = [random_tuple[0], random_tuple[1]]
+    next_key = random_tuple
+    while random_value != ".":
+        tuple_values = chains[next_key]
+        random_value = random.choice(tuple_values)
+        markov_sent.append(random_value)
+        next_key = (next_key[1], random_value)
+
+    # print markov_sent
+
+    #writing a join as a loop
+    #markov_sent is a list! We're going to concatinate the strings in this list. 
+    # for word in markov_sent:
+
+
+    # str = " "
+    # return str.join(markov_sent)
 
 def main():
     args = sys.argv
@@ -46,7 +69,7 @@ def main():
 
     chain_dict = make_chains(file_string)
     random_text = make_text(chain_dict)
-    print random_text
+    #print random_text
 
 
 if __name__ == "__main__":
