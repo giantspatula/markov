8bcf0fca762c021516e6bb4de7a341866738f404
giantspatula <cwmalloy@gmail.com>
2014-10-09 08:26:14 -0700

    Worked on adding a clean library and making command line arguments flexible. Now generates chains for game and movie scripts.

---
+++ clean.py
@@ -0,0 +1,85 @@
+import string
+import os.path 
+
+def make_clean_text(file_name):
+    """Takes in a text_file and creates a new clean_text_file clean of all punctuation and returns the clean file name as a string."""
+
+    clean_file_name = "clean_" + file_name
+
+    if not os.path.isfile(clean_file_name):
+        dirty_file_object = open(file_name)
+        dirty_file_string = dirty_file_object.read()
+        no_punct_string = remove_punctuation(dirty_file_string)
+        clean_file_object = open(clean_file_name, 'w')
+        clean_file_object.write(no_punct_string)
+        clean_file_object.close()
+    
+    return clean_file_name
+        
+def make_clean_sentence_file(file_name):
+    "Takes in a text_file and cleans it up enough to split it based on '.' latter down the line."
+
+    clean_file_name = "clean_sentence_" + file_name
+
+    if not os.path.isfile(clean_file_name):
+        dirty_file_object = open(file_name)
+        dirty_file_string = dirty_file_object.read()
+        no_punct_string = format_for_sentence_split(dirty_file_string)
+        clean_file_object = open(clean_file_name, 'w')
+        clean_file_object.write(no_punct_string)
+        clean_file_object.close()
+    
+    return clean_file_name
+
+def make_clean_script_file(file_name):
+    "Takes in a text_file and cleans it up enough to split it based on '\n' latter down the line."
+
+    clean_file_name = "clean_script_" + file_name
+
+    if not os.path.isfile(clean_file_name):
+        dirty_file_object = open(file_name)
+        dirty_file_string = dirty_file_object.read()
+        no_punct_string = format_whitespace(dirty_file_string)
+        clean_file_object = open(clean_file_name, 'w')
+        clean_file_object.write(no_punct_string)
+        clean_file_object.close()
+    
+    return clean_file_name
+
+def format_whitespace(corpus):
+    """Replaces multiple newlines with single newline."""
+
+    corpus = corpus.replace("\n\n", "\n").replace(">", "").replace("<", "").replace("[", "").replace("]", "").replace("(", "").replace(")", "").replace("\"", "")
+
+    return corpus
+
+def remove_whitespace():
+    pass
+
+def remove_punctuation(dirty_string):
+
+    clean_string = ""
+
+    for char in dirty_string:
+        if char not in string.punctuation:
+        # looking at each character in the dirty file.  If it is a punctuation included in string.punctuation, then do nothing.
+            # I do not want it in my new clean string
+            # If the character is not included in string.punctuation, then add it to a new clean string
+            clean_string = clean_string + char
+
+    return clean_string
+
+def format_for_sentence_split(corpus):
+    """Takes corpus string, returns clean text string ready to be split on '.'"""
+
+    #super fucking jank
+
+    corpus = corpus.replace("\n", " ").replace(":", " ").replace("!", ".").replace("?", ".").replace("\r", " ").replace("\t", " ").replace("  ", " ").replace ("'", "\u2018" )
+    valid_punctuation = string.letters + "." + " " + "," + ";" + "\'" + "-" + u"\u2018"
+    print valid_punctuation
+    clean_text = ""
+    for char in corpus:
+        if char in valid_punctuation:
+            clean_text = clean_text + char
+    # sentences = clean_text.split(".")
+    return clean_text
--- markov_by_sentence.py
+++ markov_by_sentence.py
@@ -4,22 +4,24 @@ import sys
 import random
 import string
 import re
+from clean import *
 
-def make_chains(corpus):
+def make_chains(corpus, split_character = "."):
     """Takes an input text as a string and returns a dictionary of
     markov chains."""
     
     chain_dict = {}
 
-    sentence_list = massage_corpus(corpus)
+    sentence_list = corpus.split(split_character)
     for sentence in sentence_list:
-        populate_dict(sentence, chain_dict)
+        populate_dict(sentence, chain_dict, split_character)
 
     return chain_dict
 
-def populate_dict(sentence, dict):
+def populate_dict(sentence, dict, split_character):
+    """ Here a sentence is defined as whatever is between our make_chains split_character. It could be ., it could be \n."""
 
-    list_of_words = sentence.split()
+    list_of_words = sentence.split() #splits on space
     if len(list_of_words) >= 2:
         last_tuple = len(list_of_words) - 2 
         for x in range(0, last_tuple):
@@ -28,52 +30,49 @@ def populate_dict(sentence, dict):
             dict[key_tuple] = dict.get(key_tuple, []) + value_list
 
         key_tuple = (list_of_words[last_tuple],list_of_words[last_tuple + 1])
-        value_list = ["."]
+        value_list = [split_character]
         dict[key_tuple] = dict.get(key_tuple, []) + value_list
     #else:
         #sentence of length 1. Ingore word. 
 
-
-def massage_corpus(corpus):
-    """Takes corpus, returns list of sentences with cleaned words"""
-
-    #super fucking jank
-
-    corpus = corpus.replace("\n", " ").replace("-", " ").replace(":", " ").replace("!", ".").replace("?", ".").replace("\r", " ").replace("\t", " ").replace("  ", " ")
-    valid_punctuation = string.letters + "." + " " + "," + ";" + "'"
-    clean_text = ""
-    for char in corpus:
-        if char in valid_punctuation:
-            clean_text = clean_text + char
-    #print clean_text
-    sentences = clean_text.split(".")
-    # for sentence in sentences:
-    #     sentence = sentence.strip()
-    return sentences
-
-def make_text(chains):
+def make_text(chains, script_formatting, split_character):
     """Takes a dictionary of markov chains and returns random text
     based off an original text."""
 
     random_value = None
     key_list = chains.keys()
     random_tuple = random.choice(key_list)
+    if script_formatting:
+        while random_tuple[0][-1] != ":" or random_tuple[0][0] not in string.uppercase:
+            random_tuple = random.choice(key_list)
+    else:
+        while random_tuple[0][0] not in string.uppercase:
+            random_tuple = random.choice(key_list)
+    print script_formatting
+    print random_tuple
     markov_sent = [random_tuple[0], random_tuple[1]]
-    while random_value != ".":
+    while random_value != split_character:
         tuple_values = chains[random_tuple]
         random_value = random.choice(tuple_values)
         markov_sent.append(random_value)
         random_tuple = (random_tuple[1], random_value)
 
-    return custom_join(markov_sent)
+    print markov_sent
 
-def custom_join(list):
+    return custom_join(markov_sent, script_formatting)
 
-    first_word = list[0]
-    joined_string = string.capitalize(first_word)
-    for x in range(1,len(list)-1):
-        joined_string = joined_string + " " + list[x]
-    joined_string = joined_string + "."
+def custom_join(list, script_formatting):
+
+    joined_string = list[0]
+    if script_formatting:
+        for x in range(1,len(list)-1):
+            if list[x][-1] == ":":
+                list[x] = "\n" + string.capitalize(list[x])
+            joined_string = joined_string + " " + list[x]
+    else:
+        for x in range(1,len(list)-1):
+            joined_string = joined_string + " " + list[x]
+        joined_string = joined_string + "."
 
     return joined_string
 
@@ -81,14 +80,36 @@ def custom_join(list):
 def main():
     args = sys.argv
 
-    program, file_name = args
-    file_object = open(file_name)
-    file_string = file_object.read()
+    if len(sys.argv) == 3:
+        program, file_name, split_arg = args
+        split_character = split_arg
+        print split_character
+    elif len(sys.argv) == 2:
+        program, file_name = args
+    else:
+        print "correct usage: markov_by_sent.py, file_name, [split_character]"
+    
+    script_formatting = False
+    split_character = split_arg
 
-    input_text = "sample.txt"
+    while True:
+        if split_character == ".":
+            print "Splitting file by", split_character
+            clean_file = make_clean_sentence_file(file_name)
+            break
+        elif split_character == "newline":
+            print "Splitting file by", split_character
+            clean_file = make_clean_script_file(file_name)
+            split_character = "\n"
+            script_formatting = True
+            break
+        else:
+           raise TypeError ("Illegal split character")
 
-    chain_dict = make_chains(file_string)
-    random_text = make_text(chain_dict)
+    file_string = open(clean_file).read()
+
+    chain_dict = make_chains(file_string, split_character)
+    random_text = make_text(chain_dict, script_formatting, split_character)
     print random_text
 
 
